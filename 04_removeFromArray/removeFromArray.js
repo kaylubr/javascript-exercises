b06ad633d4e3eaa016a9d73ee6b3640ed2066ae9
kaylubr <kylebreyes092005@gmail.com>
2024-07-19 19:44:58 +0800

    Add function of removing something from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayInput, value, ...moreArgs) {
+    moreArgs.push(value);
+    const sortedArgs = moreArgs.sort();
+    
+    if (arguments.length > 2) {
+        for (let i = 0; i < sortedArgs.length; i++) {
+            for (let j = 0; j < arrayInput.length; j++) {
+                if (sortedArgs[i] === arrayInput[j]) {
+                    arrayInput.splice(j, 1);
+                    j = 0;
+                }
+            }
+          }
+
+        return arrayInput;
+    }
+
+    for (let index = 0; index < arrayInput.length; index++) {
+      if (arrayInput[index] === value) {
+        arrayInput.splice(index, 1);
+        index = 0;
+      }
+    }
+
+    return arrayInput;
 };
 
 // Do not edit below this line
