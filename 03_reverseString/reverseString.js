b7c3f7e4a64b258a9e03de5be004df49dfaa4183
kaylubr <kylebreyes092005@gmail.com>
2024-07-19 17:52:11 +0800

    Add a function for reversing a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reversedWord = "";
+    for (let index = word.length; index > 0; index--) {
+        reversedWord += word[index - 1];
+    }
 
+    return reversedWord;
 };
 
 // Do not edit below this line
