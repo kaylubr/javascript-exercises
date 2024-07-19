10558bce6eb4aa2182bed9c9c8b16ab14c5a2178
kaylubr <kylebreyes092005@gmail.com>
2024-07-19 17:40:44 +0800

    Add a function that concatenate multiple strings based on the given argument

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    let emptyWord = "";
+    for (let i = 0; i < num; i++) {
+        emptyWord += word;
+    }
+
+    return emptyWord;
 };
 
 // Do not edit below this line
