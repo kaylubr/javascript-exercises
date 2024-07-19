c674c88a51c5b392cc3bb147472c029f3f49df1d
kaylubr <kylebreyes092005@gmail.com>
2024-07-19 20:45:48 +0800

    Add a function the converts fahrenheit to celcius and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  const temp = parseFloat(((temperature - 32) * (5/9)).toFixed(1));
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const temp = parseFloat((temperature * (9/5) + 32).toFixed(1));
+  return temp;
 };
 
 // Do not edit below this line
