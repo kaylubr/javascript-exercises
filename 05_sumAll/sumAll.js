7b13ec73d9aa77cecdf9dbc9aca8eb24be8c2318
kaylubr <kylebreyes092005@gmail.com>
2024-07-19 20:12:09 +0800

    Add a function that sum the arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof num1 == 'string' || typeof num2 == 'string') return "ERROR";
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (!Number.isInteger(num1, num2)) return "ERROR";
+    if (Array.isArray(num1) || Array.isArray(num2)) return "ERROR";
 
+    let sum = 0;
+
+    if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = num2; i <= num1; i++) {
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
